278fd1cea3f2c93d9bf878f0e32bcb448672b65b
ibukamshindi <ibukamshindi@gmail.com>
2020-10-12 03:51:31 +0300

    add pillow update settings

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
-from . import views 
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.my_photos,name='myProjects'),
+    url(r'^$',views.my_photos,name='myPhotos'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
