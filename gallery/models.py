60f20470cf75adc1a4409f916fb6f85006a27917
ibukamshindi <ibukamshindi@gmail.com>
2020-10-12 06:36:58 +0300

    add and methods

--- gallery/models.py
+++ gallery/models.py
@@ -6,8 +6,20 @@ import datetime as dt
 class Location(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
 class categories(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
 
 class Photos(models.Model):
     image = models.ImageField(upload_to = 'photos/', null = True)
@@ -17,7 +29,8 @@ class Photos(models.Model):
     category = models.ManyToManyField(categories)
     time_uloaded = models.DateTimeField(auto_now_add=True, null=True)
 
-
+    def save_image(self):
+        self.save()
 
 
 
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Photos, categories, Location
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name = 'Ruiru')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+class CategoriesTestClass(TestCase):
+    def setUp(self):
+        self.category = categories(name='project')
+
+    def test_category_instance(self):
+        self.assertTrue(isinstance(self.category, categories))
+
+    def test_save_method(self):
+        self.category.save_category()
+        categories_object= categories.objects.all()
+        self.assertTrue(len(categories_object)>0)
+
+
+    
+
+class PhotosTestClass(TestCase):
+    def setUp(self):
+        self.photo = Photos(image='imageurl', name='project', descripton='the intial step')
+
+    def test_photo_instance(self):
+        self.assertTrue(isinstance(self.photo, Photos))
+
+    def test_save_photo_method(self):
+        self.photo.save_image()
+        photos = Photos.objects.all()
+        self.assertTrue(len(photos)>0)
